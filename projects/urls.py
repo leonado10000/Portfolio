524bc94069960ea2098890bcf53738031cb35ac6
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2026-03-27 00:00:58 +0530

    added page for schoolmate

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import codebunny_landing
+from .views import codebunny_landing, schoolmate_landing
 
 urlpatterns = [
-    path("codebunny", codebunny_landing, name="codebunny_landing")
+    path("codebunny", codebunny_landing, name="codebunny_landing"),
+    path("schoolmate", schoolmate_landing, name="schoolmate_landing"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from utils.bronzelogger import bronzelogger
 
 # Create your views here.
+@bronzelogger
 def codebunny_landing(request):
     return render(request, "codebunny/codebunny_landing.html")
+
+@bronzelogger
+def schoolmate_landing(request):
+    return render(request, "schoolmate/schoolmate_landing.html")
+
+@bronzelogger
+def sockets_chatroom_landing(request):
+    return render(request, "sockets_chatroom/sockets_chatroom_landing.html")
+
+@bronzelogger
+def autograd_from_scratch_landing(request):
+    return render(request, "autograd_from_scratch/autograd_from_scratch_landing.html")
